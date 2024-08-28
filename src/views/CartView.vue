62caf28101b08efaa3b6ba3774d721f086183a68
kirito820 <2317432392@qq.com>
2024-08-28 14:24:51 +0800

    在表格中显示序号

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,34 +1,40 @@
 <script>
 export default {
     data() {
-
+        return {
+            goods: [
+                { name: "matebook x", price: 8999.00, amount: 2 },
+                { name: "xiaomi book", price: 6999.00, amount: 2 }
+            ]
+        }
     },
     computed: {
-        totalPrice() {
-            let sum=0;
-            for(let i=0;i<this.goods.length;i++) {
-                let g=this.goods[i];
-                let subTotal
+        totalPrice(){
+            let sum = 0;
+            for(let i = 0; i < this.goods.length; i++ ) {
+                let g = this.goods[i];
+                let subTotal = g.price * g.amount;
+                sum = sum + subTotal;
             }
+            return sum;
         }
     }
 }
 </script>
+
 <template>
     <div>
         <h5>购物车</h5>
         <el-table :data="goods">
+            <el-table-column type="index" label="序号" width="100"/>
+            <el-table-column label="商品名称" prop="name"/>
+            <el-table-column label="单价" prop="price"/>
+            <el-table-column label="数量" prop="amount"/>
             <el-table-column>
-                <template #header>序号</template>
+                <template #header>小计</template>
                 <template #default>如之奈何？</template>
             </el-table-column>
-            <el-table-column label="商品名称" prop="name"></el-table-column>
-            <el-table-column label="单价" prop="price"></el-table-column>
-            <el-table-column label="数量" prop="amount"></el-table-column>
-            <el-table-column label="小计" prop=""></el-table-column>
-            <el-table-column label="" prop=""></el-table-column>
-            <el-table-column></el-table-column>
-            <el-table-column></el-table-column>
         </el-table>
+        <p>总价: {{ totalPrice }}</p>
     </div>
 </template>
